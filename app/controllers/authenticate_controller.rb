c8ef64f11739eeba00222ee180d0e9cccde03e33
Shuflduf <shuflduf@gmail.com>
2025-11-03 00:54:11 -0700

    i fucking cant with this https bullshit

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -9,6 +9,23 @@ class AuthenticateController < ApplicationController
   def slackcallback
     @code = params[:code]
     puts @code
+
+    require 'faraday'
+    conn = Faraday.new(
+      url: 'https://slack.com/',
+      headers: {'Content-Type' => 'application/json'}
+    )
+
+    response = conn.post('/api/openid.connect.token') do |req|
+      req.body = {'code' => @code, 'client_secret' => 'value2', 'client_id' => 'value3', 'redirect_uri' => 'value4'}.to_json
+    end
+
+    puts response.body
+    # uri = URI.parse("https://slack.com/api/openid.connect.token")
+    # request.set_form_data({'code' => 'value1', 'client_secret' => 'value2', 'client_id' => 'value3', 'redirect_uri' => 'value4'})
+
     redirect_to action: "login"
+
+    
   end
 end
