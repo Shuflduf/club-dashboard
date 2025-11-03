541018d0f06e8534f25be4e42978610305d4a0b0
Shuflduf <shuflduf@gmail.com>
2025-11-03 11:45:32 -0700

    oisafyhds

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -1,3 +1,5 @@
+require "faraday"
+
 class AuthenticateController < ApplicationController
   def signup
   end
@@ -6,22 +8,20 @@ class AuthenticateController < ApplicationController
   end
 
   def slackcallback
-    code = params[:code]
-    slack_client_id = "2210535565.9831435111892"
-    slack_client_secret = Rails.application.credentials.slack_client_secret
-    redirect_uri = "https%3A%2F%2Flocalhost%3A3001%2Fauth%2Fslack%2Fcallback"
-    puts code
+    request_params = {
+      code: params[:code],
+      client_id: "2210535565.9831435111892",
+      client_secret: Rails.application.credentials.slack_client_secret,
+      redirect_uri: "https%3A%2F%2Flocalhost%3A3001%2Fauth%2Fslack%2Fcallback"
+    }
 
-    require "faraday"
     conn = Faraday.new(
       url: "https://slack.com",
       headers: { "Content-Type" => "application/x-www-form-urlencoded" }
     )
-
-    request_uri = "/api/openid.connect.token?code=#{code}&client_id=#{slack_client_id}&client_secret=#{slack_client_secret}&redirect_uri=#{redirect_uri}"
+    request_uri = "/api/openid.connect.token?#{request_params.to_query}"
     puts request_uri
-    response = conn.post(request_uri) do |req|
-    end
+    response = conn.post(request_uri)
 
     puts response.body
 
