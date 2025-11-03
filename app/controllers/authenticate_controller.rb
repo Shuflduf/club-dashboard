46647a848d0873499219d5c05f2b256d6245f900
Shuflduf <shuflduf@gmail.com>
2025-11-02 23:45:42 -0700

    got a redirect to finally fucking work

--- app/controllers/authenticate_controller.rb
+++ app/controllers/authenticate_controller.rb
@@ -1,10 +1,14 @@
 class AuthenticateController < ApplicationController
   def signup
+
   end
 
   def login
   end
 
   def slackcallback
+    @code = params[:code]
+    puts @code
+    redirect_to action: "login"
   end
 end
